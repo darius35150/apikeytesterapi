871f0acc82f3e5138776813bf890e29567c4767c
darius35150 <darius35150@yahoo.com>
2021-11-28 00:56:12 -0600

    first commit

---
+++ index.js
@@ -0,0 +1,92 @@
+let placename = "";
+let key = "";
+const OPTION_VALUE_BLANK = "";
+
+const PORT = process.env.PORT || 3000;
+const express = require('express');
+const axios = require('axios');
+const { response } = require('express');
+const app = express();
+
+
+app.get('/googlegeocode', (req, res) => {
+
+  performGeocodeLookup(req, res);
+
+});
+
+app.get('/googleplaces', (req, res) => {
+  performPlacesLookup(req, res);
+});
+
+app.listen(PORT, () =>{
+
+    console.log(`Listening on port ${PORT}`);
+
+});
+
+const performGeocodeLookup = (req, res) => {
+  key = req.query.key;
+  placename = req.query.placename;
+  if (!testMeetsQualifications(key))
+  {
+    return  res.json({
+      "error":  "Key and type are missing from request"
+    });
+  }
+
+  performGeocodeTest(key, placename, req, res);
+}
+
+const performGeocodeTest = (key, placename, req, res) => {
+
+  console.log(`Key ${key}`);
+  console.log(`Placename ${placename}`);
+  if(placename === OPTION_VALUE_BLANK)
+  {
+    return res.json({"error" : "Missing paramenter placename"});
+  }
+
+  axios.get("https://maps.googleapis.com/maps/api/geocode/xml?address=" + placename + "&key=" + key).then(response =>{
+    console.log(response["data"]);
+    res.send(response["data"]);
+
+  });
+}
+
+const performPlacesLookup = (res, req) =>{
+  key = req.query.key;
+  placename = req.query.placename;
+  if (!testMeetsQualifications(key))
+  {
+    return  res.json({
+      "error":  "Key and type are missing from request"
+    });
+  }
+
+  performPlacesTest(key, placename, req, res);
+
+}
+
+const performPlacesTest = (key, placename, req, res) =>{
+
+  console.log(`Key ${key}`);
+  console.log(`Placename ${placename}`);
+  if(placename === OPTION_VALUE_BLANK)
+  {
+    return res.json({"error" : "Missing paramenter placename"});
+  }
+
+  axios.get("https://maps.googleapis.com/maps/api/place/textsearch/xml?query=" + placename + "&key=" + key).then(response =>{
+    console.log(response["data"]);
+    res.send(response["data"]);
+
+  });
+
+}
+
+const testMeetsQualifications = (key) =>{
+
+  return (key != "");
+
+}
